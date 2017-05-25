1ec02ee9915529391a8a6d0a21481185a59b4e14
jagraj <jagraj@us.ibm.com>
2017-05-25 10:17:16 -0400

    Pull request for ConfigAPI changes and use Cloudant stable version. (#4)
    * Updates to ConfigAPI changes and switch to older Cloudant image

--- src/main/java/io/microprofile/showcase/vote/persistence/couch/CredentialsProducer.java
+++ src/main/java/io/microprofile/showcase/vote/persistence/couch/CredentialsProducer.java
@@ -26,16 +26,18 @@ import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonString;
 
-import org.eclipse.microprofile.config.ConfigProperty;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
 
 @ApplicationScoped
 public class CredentialsProducer {
 	
-	@Inject @ConfigProperty("dbUsername") String username;
-	@Inject @ConfigProperty("dbPassword") String password; 
-	@Inject @ConfigProperty("dbUrl") String url;
-	@Inject @ConfigProperty("CLOUDANT_SERVICE_SERVICE_HOST") String dbHost;
-	@Inject @ConfigProperty("CLOUDANT_SERVICE_SERVICE_PORT") String dbPort;
+	@Inject @ConfigProperty(name="dbUsername") String username;
+	@Inject @ConfigProperty(name="dbPassword") String password; 
+	@Inject @ConfigProperty(name="dbUrl") String url;
+	@Inject @ConfigProperty(name="CLOUDANT_SERVICE_SERVICE_HOST") String dbHost;
+	@Inject @ConfigProperty(name="CLOUDANT_SERVICE_SERVICE_PORT") String dbPort;
 	//private String url;
 	
 	
